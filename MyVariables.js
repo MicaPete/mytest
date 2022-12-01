e6877a79d0447da730caf05d00e050af4b9bee32
Meike Peters <mepeters@adobe.com>
2022-12-01 10:08:35 -0700

    changes

--- MyVariables.js
+++ MyVariables.js
@@ -1,2 +1,7 @@
 var s_account = "adobecx1meike";
 var s = s_gi(s_account);
+s.charSet = "UTF-8";
+s.trackingServer = "meike.data.adobedc.net";
+s.pageName = "Meike's home page";
+s.eVar1 = "Example value";
+s.prop1 = "Prop";
